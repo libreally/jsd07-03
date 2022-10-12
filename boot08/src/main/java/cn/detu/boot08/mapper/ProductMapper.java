1f6935bd434323d8d7aa7060e6d3acbffb5a2329
libreally <59366478+libreally@users.noreply.github.com>
2022-10-12 20:52:24 +0800

    Update ProductMapper.java

--- boot08/src/main/java/cn/detu/boot08/mapper/ProductMapper.java
+++ boot08/src/main/java/cn/detu/boot08/mapper/ProductMapper.java
@@ -4,7 +4,7 @@ import cn.detu.boot08.entity.Product;
 import org.springframework.stereotype.Repository;
 
 
-//此时不需要@Mapper注解
+//此时不需要@Mapper注解 只写抽象方法
 public interface ProductMapper {
 
     void insert(Product product);
