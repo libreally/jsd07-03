347e1c130ee6ddc81e395e8d98a85182965baa7c
Administrator <2359304261@qq.com>
2022-10-12 11:50:44 +0800

    c7

---
+++ boot0701/src/main/java/cn/tedu/boot0701/Boot0701Application.java
@@ -0,0 +1,13 @@
+package cn.tedu.boot0701;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boot0701Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boot0701Application.class, args);
+    }
+
+}
