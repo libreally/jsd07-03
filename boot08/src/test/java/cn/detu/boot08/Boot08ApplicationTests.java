679005963dcad68e522b195072f979cccc207d09
Administrator <2359304261@qq.com>
2022-10-12 19:05:57 +0800

    c8

---
+++ boot08/src/test/java/cn/detu/boot08/Boot08ApplicationTests.java
@@ -0,0 +1,19 @@
+package cn.detu.boot08;
+
+import cn.detu.boot08.entity.Product;
+import cn.detu.boot08.mapper.ProductMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+
+@SpringBootTest
+class Boot08ApplicationTests {
+
+    @Test
+    void contextLoads() {
+
+    }
+
+}
